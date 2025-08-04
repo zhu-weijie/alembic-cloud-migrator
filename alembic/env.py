9ac1fd8624868c07c87ff5fa2b99cd3c8cedfb81
zhuwj <byzhubaiyuan@gmail.com>
2025-08-04 17:06:33 +0800

    Correct the alembic/env.py File

--- alembic/env.py
+++ alembic/env.py
@@ -35,7 +35,7 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     connectable = engine_from_config(
-        config.get_section(config.config_main_section, {}),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
