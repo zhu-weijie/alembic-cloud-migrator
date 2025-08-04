089346818d6762053954a9e406c160a7644aad49
zhuwj <byzhubaiyuan@gmail.com>
2025-08-04 20:49:01 +0800

    Add Database and ECS Task

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,3 +29,25 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# This inline policy grants the task permission to read the specific secret
+# containing the database credentials.
+resource "aws_iam_role_policy" "secrets_access" {
+  name = "SecretsManagerAccessPolicy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Effect = "Allow",
+        Resource = [
+          aws_secretsmanager_secret.db_credentials.arn
+        ]
+      }
+    ]
+  })
+}
